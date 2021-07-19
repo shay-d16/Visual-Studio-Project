f2c82d662a62291df9f98941c2d4e8150d811148
Shay R <shalondra.davis@protonmail.com>
2021-07-19 14:38:47 -0700

    Complete the requirements for this project.

--- MyFavoriteSong/wwwroot/js/site.js
+++ MyFavoriteSong/wwwroot/js/site.js
@@ -2,3 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+window.onload = function () {
+    document.getElementById('fade-in').style.opacity = 1;
+}
